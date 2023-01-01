270dd22b6e8852b0b0de4a8fd916fe262d201dfe
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-01-01 14:07:31 +0200

    create contact on hooks

--- src/components/CreateContact/CreateContact.jsx
+++ src/components/CreateContact/CreateContact.jsx
@@ -2,76 +2,128 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Box } from '../Box';
 import { EnterLabel, EnterInput, StyledButton } from '../App.styled';
+import { useState } from 'react';
 
-class CreateContact extends Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired,
-    number: PropTypes.string.isRequired,
-  };
+const CreateContact = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  state = {
-    name: '',
-    number: '',
-  };
-
-  handleChange = e => {
-    this.setState({
-      [e.currentTarget.name]: e.currentTarget.value,
-    });
-  };
-  handleSubmit = e => {
-    const { name, number } = this.state;
+  const handleChangeName = e => setName(e.currentTarget.value);
+  const handleChangeNumber = e => setNumber(e.currentTarget.value);
+  const handleSubmit = e => {
     e.preventDefault();
-
-    this.props.onSubmit(name, number);
-    this.setState({
-      name: '',
-      number: '',
-    });
-    // e.target.name.value = '';
-    // e.target.number.value = '';
+    console.log('all work');
+    onSubmit(name, number);
+    setName('');
+    setNumber('');
   };
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="flex-start"
+      as="form"
+      onSubmit={handleSubmit}
+    >
+      <EnterLabel>
+        Name:
+        <EnterInput
+          type="text"
+          name="name"
+          value={name}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={handleChangeName}
+        />
+      </EnterLabel>
 
-  render() {
-    const { name, number } = this.state;
+      <EnterLabel>
+        Number:
+        <EnterInput
+          type="tel"
+          name="number"
+          value={number}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          onChange={handleChangeNumber}
+        />
+      </EnterLabel>
+      <StyledButton type="submit">Add contact</StyledButton>
+    </Box>
+  );
+};
 
-    return (
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="flex-start"
-        as="form"
-        onSubmit={this.handleSubmit}
-      >
-        <EnterLabel>
-          Name:
-          <EnterInput
-            type="text"
-            name="name"
-            value={name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleChange}
-          />
-        </EnterLabel>
+// class CreateContact extends Component {
+//   static propTypes = {
+//     name: PropTypes.string.isRequired,
+//     number: PropTypes.string.isRequired,
+//   };
 
-        <EnterLabel>
-          Number:
-          <EnterInput
-            type="tel"
-            name="number"
-            value={number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange={this.handleChange}
-          />
-        </EnterLabel>
-        <StyledButton type="submit">Add contact</StyledButton>
-      </Box>
-    );
-  }
-}
+//   state = {
+//     name: '',
+//     number: '',
+//   };
+
+//   handleChange = e => {
+//     this.setState({
+//       [e.currentTarget.name]: e.currentTarget.value,
+//     });
+//   };
+//   handleSubmit = e => {
+//     const { name, number } = this.state;
+//     e.preventDefault();
+
+//     this.props.onSubmit(name, number);
+//     this.setState({
+//       name: '',
+//       number: '',
+//     });
+//     // e.target.name.value = '';
+//     // e.target.number.value = '';
+//   };
+
+//   render() {
+//     const { name, number } = this.state;
+
+//     return (
+//       <Box
+//         display="flex"
+//         flexDirection="column"
+//         alignItems="flex-start"
+//         as="form"
+//         onSubmit={this.handleSubmit}
+//       >
+//         <EnterLabel>
+//           Name:
+//           <EnterInput
+//             type="text"
+//             name="name"
+//             value={name}
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//             required
+//             onChange={this.handleChange}
+//           />
+//         </EnterLabel>
+
+//         <EnterLabel>
+//           Number:
+//           <EnterInput
+//             type="tel"
+//             name="number"
+//             value={number}
+//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+//             required
+//             onChange={this.handleChange}
+//           />
+//         </EnterLabel>
+//         <StyledButton type="submit">Add contact</StyledButton>
+//       </Box>
+//     );
+//   }
+// }
 
 export default CreateContact;
